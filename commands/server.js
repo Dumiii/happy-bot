640718fe4ed2b8d5bdeca4640814cf6541a0da64
Dumiii <37809750+Dumiii@users.noreply.github.com>
2019-07-04 19:05:33 +0100

    Delete server.js

--- commands/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	name: 'server',
-	description: 'Provides useful information about the server.',
-	guildOnly: true,
-	execute(message, args) {
-		if(message.guild.available) 
-			return message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}\nCreated at: ${message.guild.createdAt}`);
-	},
-};
